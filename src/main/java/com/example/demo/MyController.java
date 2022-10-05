d84c36b9633edcb16e98b80a63220b8287fd2a83
ZhongYah <s763460336@gmail.com>
2022-10-05 17:29:59 +0800

    PostConstruct(初始化Bean)

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +9,6 @@ public class MyController {
 
 
     @Autowired
-    @Qualifier("yourPrinter")
     private  Printer printer;
 
     @RequestMapping("/test")
