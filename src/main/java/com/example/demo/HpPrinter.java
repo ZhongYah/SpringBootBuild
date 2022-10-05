f3a5c62c64a23caf373ff7c53f4ed381d69bfae6
ZhongYah <s763460336@gmail.com>
2022-10-05 18:41:20 +0800

    application.properties + @Value

--- src/main/java/com/example/demo/HpPrinter.java
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -1,23 +1,27 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 
 @Component
 public class HpPrinter implements Printer {
 
+    @Value("${printer.name}")
+    private String name;
+
+    @Value("${printer.count:20}")
     private int count;
 
-    @PostConstruct
-    public void  init(){
-        count = 5;
-    }
+//    @PostConstruct
+//    public void  init(){
+//        count = 5;
+//    }
 
     @Override
     public void print(String message) {
         count--;
-        System.out.println("Hp印表機 :  " + message);
+        System.out.println(name + ": " + message);
         System.out.println("剩餘使用次數: " +  count);
     }
 }
