bd9aaae02ac3da7f1a4195e066e9e5c8305037e5
ZhongYah <s763460336@gmail.com>
2022-10-07 17:20:51 +0800

    URL對應路徑

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,19 +1,19 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/detail")
 @RestController
 public class MyController {
 
+    @RequestMapping("/product")
+    public String product(){
+        return "第一個是蘋果、第二個是橘子";
+    }
 
-    @Autowired
-    private  Printer printer;
-
-    @RequestMapping("/test")
-    public String test(){
-       printer.print("Hello World !");
-        return "Hello World";
+    @RequestMapping("user")
+    public String user(){
+        return "名字為Jay";
     }
 }
