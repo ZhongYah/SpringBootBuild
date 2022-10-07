f4c8f23866fa3be49644d9596f8cc84be9636dc5
ZhongYah <s763460336@gmail.com>
2022-10-07 15:24:35 +0800

    Spring AOP用法 & @Aspect

--- src/main/java/com/example/demo/HpPrinter.java
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -1,27 +1,30 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class HpPrinter implements Printer {
 
-    @Value("${printer.name}")
-    private String name;
+    @Override
+    public void print(String message){
+        System.out.println("HP印表機: " + message);
 
-    @Value("${printer.count:20}")
-    private int count;
+//    @Value("${printer.name}")
+//    private String name;
+//
+//    @Value("${printer.count:20}")
+//    private int count;
 
 //    @PostConstruct
 //    public void  init(){
 //        count = 5;
 //    }
 
-    @Override
-    public void print(String message) {
-        count--;
-        System.out.println(name + ": " + message);
-        System.out.println("剩餘使用次數: " +  count);
+//    @Override
+//    public void print(String message) {
+//        count--;
+//        System.out.println(name + ": " + message);
+//        System.out.println("剩餘使用次數: " +  count);
     }
 }
