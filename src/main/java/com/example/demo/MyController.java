3746d707506562961762acc13a0791bf4424b616
ZhongYah <s763460336@gmail.com>
2022-10-12 22:15:15 +0800

    @ RequestParam

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,31 +1,21 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class MyController {
 
-    @RequestMapping("/product")
-    public Store product() {
-        Store store = new Store();
-        List<String> List = new ArrayList<>();
-        List.add("蘋果");
-        List.add("橘子");
-        store.setProductList(List);
-        return store;
+    @RequestMapping("/test1")
+    public String test1(@RequestParam Integer id,
+                                @RequestParam(defaultValue = "Bob") String name) {
+        System.out.println("id:" + id);
+        System.out.println("name: " + name);
+        return "Hello test1";
     }
-
-    @RequestMapping("/user")
-    public Student user() {
-        Student student = new Student();
-        student.setName("Jay");
-        student.setId(123);
-        return student;
-    }
-
 }
+
+
+
 
