a43e7bd24d2b3c44f7371238b448b2163299efb6
ZhongYah <s763460336@gmail.com>
2022-10-20 10:25:04 +0800

    攔截器 Interceptor

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -10,11 +10,13 @@ public class MyController {
 
     @RequestMapping("/test1")
     public String test1(){
-        throw new RuntimeException("test error");
+        System.out.println("執行 test1 方法");
+        return "Hello test1";
     }
 
     @RequestMapping("/test2")
     public String test2(){
-        throw new IllegalArgumentException("test2 error");
+        System.out.println("執行 test2 方法");
+        return "Hello test2";
     }
 }
